f1a666b2a44c52ecb89f6819c66ad1bbe48f4001
SSuSG <psg980331@naver.com>
2022-01-12 13:44:24 +0900

    initial commit

---
+++ src/test/java/SongGyun/ShoppingMallProject/ShoppingMallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SongGyun.ShoppingMallProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
